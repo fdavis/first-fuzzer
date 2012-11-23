dfcb94a8e7e50a24a2ca5b9163f2747bb1e7fbd2
root <root@localhost.localdomain>
2012-11-23 16:44:07 +0000

    modifications made from my VM
    Made the outer loop infinite
    Made the oracle work with nano
    Emacs gave me trouble with the X, should've switched to non graphical
    Ran the example_pull_files with slightly different arguments

--- example_file_grab.sh
+++ example_file_grab.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./pull_files.py -v -d ~ -t ./files -e txt,asm,sh,py,pl,c,cpp,h,conf,src,php,tcl,htm,html,vhd,vhdl,log,xml,json,lua,tex 
+./pull_files.py -d / -t ./files -e txt,asm,sh,py,pl,c,cpp,h,conf,src,php,tcl,htm,html,vhd,vhdl,log,xml,json,lua,tex 
--- first_fuzzer.py
+++ first_fuzzer.py
@@ -11,16 +11,24 @@ apps = [
     "/usr/bin/gedit",
     "/bin/ed",
     "/usr/bin/w3m",
-    "/usr/bin/emacs",
-    "/usr/bin/bluefish",
+#    "/usr/bin/emacs",
+#    "/usr/bin/bluefish",
     "/bin/nano",
     "/usr/bin/vim"
     ]
 
 fuzz_output = "fuzz.txt"
 
+fuzzes = [ 	10,
+		27,
+		47,
+		51,
+		100,
+	   	200,
+		250
+	 ]
 
-FuzzFactor = 250
+#FuzzFactor = 250
 num_tests = 100
 
 tempf = "./temp"
@@ -39,8 +47,10 @@ from shutil import copyfile
 
 nil = open('/dev/null', 'r')
 
-for i in range(num_tests):
+while True:
+#for i in range(num_tests):
     file_choice = random.choice(file_list)
+    FuzzFactor = random.choice(fuzzes)
 #    app = random.choice(apps)
 
     buf = bytearray(open(file_choice, 'rb').read())
@@ -49,7 +59,7 @@ for i in range(num_tests):
     try:
         numwrites=random.randrange(math.ceil((float(len(buf)) / FuzzFactor)))+1
     except ValueError as ve:
-        print "Skipping empty file"
+#        print "Skipping empty file"
         continue
 
     for j in range(numwrites):
@@ -62,7 +72,7 @@ for i in range(num_tests):
 
     for app in apps:
         f = open(tempf, 'w')
-        print app
+#        print app
         try:
             process = subprocess.Popen([app, fuzz_output], stderr=subprocess.STDOUT, \
                                    stdout=f, stdin=nil, bufsize=-1)
@@ -71,7 +81,8 @@ for i in range(num_tests):
 #        print process, app 
         time.sleep(1)
         crashed = process.poll()
-        if not crashed:
+        # nano returns 1 on sigs term and hup.. why is this a problem?
+        if not crashed or app == "/bin/nano" and crashed == 1:
             try:
                 process.terminate()
             except OSError:
@@ -80,6 +91,7 @@ for i in range(num_tests):
 #                print app, f.read()
             f.close()
         else:
+            print 'else saving results...', app
             f.close()
             dt = datetime.datetime.now().strftime("%Y%m%d.%H%M%S.%f")
             fname = res_dir + os.path.basename(app) + ".ttyout" + dt
@@ -89,3 +101,4 @@ for i in range(num_tests):
 
 
 nil.close()
+
